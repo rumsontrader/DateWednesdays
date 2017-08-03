3068a432932110865862bc3e2226db06ab4cfdea
Jim Anderson <rumsontrader@gmail.com>
2017-08-03 15:52:31 -0400

    Additional changes to datewednesdays project

--- Program.cs
+++ Program.cs
@@ -10,9 +10,28 @@ namespace DateWednesdays
     {
         static void Main()
         {
+                  
             DateTime jan1 = new DateTime(2017, 1, 1);
+            DateTime nextDay = new DateTime(2017,1,2);
+            string wednesday = "Wed";
+
+
             Console.WriteLine(jan1);
-            Console.WriteLine(jan1 == DateTime.Today);           
+
+            Console.WriteLine(jan1 == DateTime.Today);
+
+            Console.WriteLine(jan1.ToString("ddd"));
+
+            do
+            { nextDay++;
+                if{
+                    nextDay.ToString("ddd") = wednesday
+                        Console.WriteLine("Today is Wednesday " + nextDay);
+                }
+                while nextDay < DateTime.Today();
+            }
+
+
             Console.ReadLine();
         }
     }
