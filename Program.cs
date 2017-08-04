55dce4d76c23422fdbfab4ba82a071f6e4eeb0a1
Jim Anderson <rumsontrader@gmail.com>
2017-08-04 12:55:09 -0400

    update files to push to Github

--- Program.cs
+++ Program.cs
@@ -13,23 +13,30 @@ namespace DateWednesdays
                   
             DateTime jan1 = new DateTime(2017, 1, 1);
             DateTime nextDay = new DateTime(2017,1,2);
-            string wednesday = "Wed";
+            //string wednesday = "Wed";
+            string theDay = "";
+            DateTime today = DateTime.Today;
 
 
-            Console.WriteLine(jan1);
+            //Console.WriteLine(jan1);
 
-            Console.WriteLine(jan1 == DateTime.Today);
+            //Console.WriteLine(jan1 == DateTime.Today);
 
-            Console.WriteLine(jan1.ToString("ddd"));
+            //Console.WriteLine(jan1.ToString("ddd"));
 
             do
-            { nextDay++;
-                if{
-                    nextDay.ToString("ddd") = wednesday
-                        Console.WriteLine("Today is Wednesday " + nextDay);
+            { nextDay.AddDays(1);
+                //Console.WriteLine(nextDay.ToString());
+                theDay = nextDay.ToString("ddd");
+                Console.WriteLine(theDay);
+            
+                if (theDay == "Wed")
+                {
+                    Console.WriteLine("Today is Wednesday " + nextDay.ToString());
                 }
-                while nextDay < DateTime.Today();
-            }
+                nextDay.AddDays(1);
+
+            } while (nextDay < today);
 
 
             Console.ReadLine();
